1a83b6b745c9b937c9011fc3c02b1cdf980a05a7
Nattanon Siripaisananusorn <139682335+Nattan0n@users.noreply.github.com>
2024-09-06 20:38:43 +0700

    Auto Text

--- src/component/Content/Content01.jsx
+++ src/component/Content/Content01.jsx
@@ -1,11 +1,32 @@
+// Content01.jsx
+
 import { Component } from 'react';
-import '../../style/animation.css'
+import Typed from 'typed.js';
+import '../../style/animation.css';
 import nattanon_02 from '/img/nattanon_02.jpg';
 
 class Content01 extends Component {
+  componentDidMount() {
+    const element = document.querySelector('.typed-text');
+
+    if (!element._typed) { // ตรวจสอบว่ามีการสร้าง Typed.js แล้วหรือไม่
+      const options = {
+        strings: ['Computer Science', 'Front-End Developer'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        backDelay: 1000,
+        startDelay: 100,
+        loop: true,
+        cursorChar: '|' // ซ่อน cursor
+      };
+      new Typed(element, options);
+      element._typed = true; // ใช้ตัวแปรกำหนดเองเพื่อป้องกันการสร้างซ้ำ
+    }
+  }
+
   render() {
     const labelTextContent = `
-      Hi, <br>I'm <span style="color: #FFF96D">Nattanon</span> Siripaisananusorn <br>Computer Science
+      Hi, <br>I'm <span style="color: #FFF96D">Nattanon</span> Siripaisananusorn <br><span class="typed-text"></span>
       <div class="button__style" id="scrollButton_5">
           <p class="btn__text--style" id="scrollButton_5">Contact</p>
       </div>
